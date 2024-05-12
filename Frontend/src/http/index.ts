d999cf71edfe47cc3ee30114af908f8b3231202c
Gabriel Braga Estefanski <gbraga2004@outlook.com>
2024-05-12 18:45:14 -0300

    testing run dev for backend

--- Frontend/src/http/index.ts
+++ Frontend/src/http/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: 'https://astounding-cassata-365edd.netlify.app/',
+    baseURL: 'http://localhost:3000/',
     headers: { 
         'Content-Type': 'application/json',
     },
